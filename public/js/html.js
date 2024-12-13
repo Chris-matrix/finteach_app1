b6e807b2c2b74bf6e8e8cf31362cc928da28b368
Chris-matrix <cwatt0046@launchapd.org>
2024-12-13 00:03:52 -0500

    update memoize

---
+++ public/js/html.js
@@ -0,0 +1,8 @@
+// Get a reference to the button
+const button = document.querySelector('button');
+
+// Add an event listener to the button
+button.addEventListener('click', () => {
+  // Display an alert message when the button is clicked
+  alert('You clicked the button! Let\'s start your financial journey.');
+});
